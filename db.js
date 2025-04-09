a6a197ebe990d3b8fa0110e08f16f127e72a282e
Reet Agarwal <139364881+reetagarwal@users.noreply.github.com>
2025-04-09 16:20:33 +0530

    made the changes

--- db.js
+++ db.js
@@ -3,13 +3,8 @@ const mongoose = require("mongoose");
 
 require("dotenv").config();  
 
-const MONGO_URI = process.env.MONGO_URI;
-mongoose.connect(MONGO_URI, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-})
-.then(() => console.log("✅ Connected to MongoDB"))
-.catch((err) => console.error("❌ MongoDB connection error:", err));
+const MONGO_URI = "add your link";
+mongoose.connect(MONGO_URI);
 
 
 const userSchema = new mongoose.Schema({
